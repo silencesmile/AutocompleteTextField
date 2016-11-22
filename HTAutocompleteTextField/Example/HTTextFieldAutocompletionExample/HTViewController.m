521269d95cff714dc5e73280482113b4f4cdfa24
mingxing.yang <mingxing.yang@zhongtoutiandi.com>
2016-11-22 15:10:07 +0800

    textField的自动补全

---
+++ HTAutocompleteTextField/Example/HTTextFieldAutocompletionExample/HTViewController.m
@@ -0,0 +1,53 @@
+//
+//  HTViewController.m
+//  HTTextFieldAutocompletionExample
+//
+//  Created by Jonathan Sibley on 12/26/12.
+//  Copyright (c) 2012 Hotel Tonight. All rights reserved.
+//
+
+#import "HTViewController.h"
+#import "HTAutocompleteManager.h"
+
+@interface HTViewController ()
+
+@end
+
+@implementation HTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Set a default data source for all instances.  Otherwise, you can specify the data source on individual text fields via the autocompleteDataSource property
+    [HTAutocompleteTextField setDefaultAutocompleteDataSource:[HTAutocompleteManager sharedManager]];
+
+    self.emailTextField.autocompleteType = HTAutocompleteTypeEmail;
+    self.emailTextField.keyboardType = UIKeyboardTypeEmailAddress;
+
+    self.favoriteColorTextField.autocompleteType = HTAutocompleteTypeColor;
+    self.favoriteColorTextField.ignoreCase = NO;
+    
+    self.NumberTexyField.autocompleteType = HTAutocompleteTypeNumber;
+
+    // Dismiss the keyboard when the user taps outside of a text field
+    UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTap:)];
+    [self.view addGestureRecognizer:singleTap];
+}
+
+- (void)viewDidUnload
+{
+    [self setEmailTextField:nil];
+    [self setFavoriteColorTextField:nil];
+
+    [super viewDidUnload];
+}
+
+- (void)handleSingleTap:(UITapGestureRecognizer *)sender
+{
+    [self.emailTextField resignFirstResponder];
+    [self.favoriteColorTextField resignFirstResponder];
+    [self.NumberTexyField resignFirstResponder];
+}
+
+@end
